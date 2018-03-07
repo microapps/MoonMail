31f9525dc165fb591e215033a2c1d1a873d01f71
David García <david.garcia@microapps.com>
2018-03-07 17:40:31 +0100

    WIP

--- campaigns-microservice/src/commandHandlers/IncrementReportCounters.js
+++ campaigns-microservice/src/commandHandlers/IncrementReportCounters.js
@@ -1,3 +1,25 @@
+import R from 'ramda';
+
+const supportedEventTypes = ['email.delivered', 'email.reported', 'email.bounced'];
+const eventTypeSupported = R.pipe(
+  R.prop('type'),
+  R.partialRight(R.contains, [supportedEventTypes])
+);
+const eventIsValid = R.pipe(
+  R.prop('payload'),
+  R.has('campaignId')
+);
+const eventsByCampaign = R.groupBy(R.path(['payload', 'campaignId']));
+const execute = function incrementReportCounters(events = []) {
+  const eventsToCount = R.pipe(
+    R.filter(eventTypeSupported),
+    R.filter(eventIsValid),
+    eventsByCampaign
+  )(events);
+  console.log(eventsToCount);
+  return Promise.resolve(true);
+};
+
 export default {
-  execute: () => true
+  execute
 };
